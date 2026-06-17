a3ca646c5d71c942dda509874967f495c9ec65d1
BafelIvanich <238101939+ktgens@users.noreply.github.com>
2026-06-17 15:32:44 +0300

    adjusted exponent alignment

--- UI/layout/ExponentNode.cpp
+++ UI/layout/ExponentNode.cpp
@@ -2,20 +2,21 @@ module;
 #include <SFML/Graphics.hpp> 
 module ExponentNode;
 
-ExponentNode::ExponentNode(std::unique_ptr<IMathNode> base, std::unique_ptr<IMathNode> power)
-    : m_base(std::move(base)), m_power(std::move(power)) {
+ExponentNode::ExponentNode(std::unique_ptr<MathRow> base, std::unique_ptr<MathRow> power)
+    : m_base(std::move(base)), m_power(std::move(power))
+{
 }
 
 MathMetrics ExponentNode::Measure() {
     MathMetrics baseM = m_base ? m_base->Measure() : MathMetrics{ 0,0,0 };
     MathMetrics powM = m_power ? m_power->Measure() : MathMetrics{ 0,0,0 };
 
-    float powerOffsetY = powM.height * 0.6f;
+    float powerOffsetY = powM.height * 0.7f;
 
     m_metrics.width = baseM.width + powM.width;
-    m_metrics.height = baseM.height + powerOffsetY;
+    m_metrics.height = std::max(baseM.height, powM.height + powerOffsetY);
 
-    m_metrics.baselineY = baseM.baselineY + powerOffsetY;
+    m_metrics.baselineY = baseM.baselineY + (m_metrics.height - baseM.height);
 
     return m_metrics;
 }
@@ -23,16 +24,25 @@ void ExponentNode::Arrange() {
     MathMetrics baseM = m_base ? m_base->m_metrics : MathMetrics{ 0,0,0 };
     MathMetrics powM = m_power ? m_power->m_metrics : MathMetrics{ 0,0,0 };
 
-    float powerOffsetY = powM.height * 0.6f;
+    float powerOffsetY = powM.height * 0.7f;
 
     if (m_base) {
-        m_base->setPosition(sf::Vector2f(0.f, powerOffsetY));
+        //сдвиг основания вниз
+        m_base->setPosition(sf::Vector2f(0.f, m_metrics.height - baseM.height));
         m_base->Arrange();
     }
     if (m_power) {
-        m_power->setPosition(sf::Vector2f(baseM.width, 0.f));
+        m_power->setPosition(sf::Vector2f(baseM.width, m_metrics.height - baseM.height - powerOffsetY));
         m_power->Arrange();
     }
+}
+
+std::string ExponentNode::ToString() const
+{
+    std::string baseStr = m_base ? m_base->ToString() : "";
+    std::string powStr = m_power ? m_power->ToString() : "";
+
+    return "(" + baseStr + ")^(" + powStr + ")";
 }
 
 void ExponentNode::draw(sf::RenderTarget& target, sf::RenderStates states) const {
@@ -40,3 +50,4 @@ void ExponentNode::draw(sf::RenderTarget& target, sf::RenderStates states) const
     if (m_base) target.draw(*m_base, states);
     if (m_power) target.draw(*m_power, states);
 }
+
